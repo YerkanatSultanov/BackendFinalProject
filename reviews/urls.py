8b646ff96e1a5292969d295845f45a60381bc5b1
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2023-05-14 20:02:04 +0600

    fixed main page

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home_page'),
-    path('register/', views.register, name='register'),
-    path('login/', views.log_in, name='login'),
-    path('logout/', views.my_logout_view, name='logout'),
-    path('book_search',  views.search_books, name='search_book'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('profile/', views.profile, name='profile'),
-    path('category/<int:cat_id>/', views.show_category, name='category')
-]
+                  path('', views.index, name='home_page'),
+                  path('register/', views.register, name='register'),
+                  path('login/', views.log_in, name='login'),
+                  path('logout/', views.my_logout_view, name='logout'),
+                  path('book_search', views.search_books, name='search_book'),
+                  path('edit_profile/', views.edit_profile, name='edit_profile'),
+                  path('profile/', views.profile, name='profile'),
+                  path('category/<int:cat_id>/', views.show_category, name='category'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
