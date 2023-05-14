05ba7980377b066c77e0f1b1d50f54a966fc326e
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2023-05-15 00:50:28 +0600

    carousel done!

--- static/script.js
+++ static/script.js
@@ -1,15 +1,19 @@
 let offset = 0;
+let num = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function(){
     offset += 240;
-    if(offset >= 690){
+    var slides = document.getElementsByClassName('borders');
+    num += 1;
+    if(slides.length === num){
+        num = 0;
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
 })
 
-// Optional: Close the dropdown when clicking outside of it
+
 window.addEventListener('click', function(event) {
   var dropdowns = document.getElementsByClassName('dropdown-content');
   for (var i = 0; i < dropdowns.length; i++) {
