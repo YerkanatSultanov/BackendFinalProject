53d485bdccb0b7d40189bfb7f87f07b7d2abc462
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2023-05-14 23:51:48 +0600

    fixed main page

--- static/script.js
+++ static/script.js
@@ -2,8 +2,8 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function(){
-    offset += 306;
-    if(offset >= 1224){
+    offset += 240;
+    if(offset >= 690){
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
